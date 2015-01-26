cbde6f134c40c36dfedbb965bc0e4279eb865ada
markpattison <mr.pattison@gmail.com>
2015-01-26 14:56:19 +0000

    Refactoring summarie

--- CricketEngine/Match.fs
+++ CricketEngine/Match.fs
@@ -27,22 +27,23 @@ module MatchFunctions =
         formatWickets (10 - wickets)
 
     let SummaryStatus rules _match =
-        match _match.State with
+        let state = _match.State
+        match state with
         | NotStarted -> "Match not started"
         | Abandoned -> "Match abandoned without a ball being bowled"
         | A_MatchDrawn _ | AB_MatchDrawn _ | ABA_MatchDrawn _ | ABB_MatchDrawn _ | ABAB_MatchDrawn _ | ABBA_MatchDrawn _ -> "Match drawn"
-        | A_Ongoing a1 -> sprintf "%s are %i for %i in their first innings" _match.TeamA a1.GetRuns a1.GetWickets
-        | A_Completed a1 when a1.IsDeclared -> sprintf "%s scored %i for %i declared in their first innings" _match.TeamA a1.GetRuns a1.GetWickets
-        | A_Completed a1 -> sprintf "%s scored %i all out in their first innings" _match.TeamA a1.GetRuns
-        | AB_Ongoing (a1, b1) when b1.GetRuns < a1.GetRuns -> sprintf "%s trail by %s with %s remaining in their first innings" _match.TeamB (formatRuns (a1.GetRuns - b1.GetRuns)) (formatWicketsLeft b1.GetWickets)
-        | AB_Ongoing (a1, b1) when b1.GetRuns = a1.GetRuns -> sprintf "%s are level with %s remaining in their first innings" _match.TeamB (formatWicketsLeft b1.GetWickets)
-        | AB_Ongoing (a1, b1) when b1.GetRuns > a1.GetRuns -> sprintf "%s lead by %s with %s remaining in their first innings" _match.TeamB (formatRuns (b1.GetRuns - a1.GetRuns)) (formatWicketsLeft b1.GetWickets)
-        | AB_CompletedNoFollowOn (a1, b1) | AB_CompletedPossibleFollowOn (a1, b1) when b1.GetRuns < a1.GetRuns -> sprintf "%s lead by %s after the first innings" _match.TeamA (formatRuns (a1.GetRuns - b1.GetRuns))
-        | AB_CompletedNoFollowOn (a1, b1) | AB_CompletedPossibleFollowOn (a1, b1) when b1.GetRuns = a1.GetRuns -> sprintf "%s are level after the first innings" _match.TeamB
-        | AB_CompletedNoFollowOn (a1, b1) | AB_CompletedPossibleFollowOn (a1, b1) when b1.GetRuns > a1.GetRuns -> sprintf "%s lead by %s after the first innings" _match.TeamB (formatRuns (b1.GetRuns - a1.GetRuns))
-        | ABA_Ongoing (a1, b1, a2) when a1.GetRuns + a2.GetRuns < b1.GetRuns -> sprintf "%s trail by %s with %s remaining in their second innings" _match.TeamA (formatRuns (b1.GetRuns - a1.GetRuns - a2.GetRuns)) (formatWicketsLeft a2.GetWickets)
-        | ABA_Ongoing (a1, b1, a2) when a1.GetRuns + a2.GetRuns = b1.GetRuns -> sprintf "%s are level with %s remaining in their second innings" _match.TeamA (formatWicketsLeft a2.GetWickets)
-        | ABA_Ongoing (a1, b1, a2) when a1.GetRuns + a2.GetRuns > b1.GetRuns -> sprintf "%s lead by %s with %s remaining in their second innings" _match.TeamA (formatRuns (a1.GetRuns + a2.GetRuns - b1.GetRuns)) (formatWicketsLeft a2.GetWickets)
-        | ABA_VictoryB (a1, b1, a2) -> sprintf "%s won by %s" _match.TeamB (formatRuns (b1.GetRuns - a1.GetRuns - a2.GetRuns))
-        | ABA_Completed (a1, b1, a2) -> sprintf "%s need %s to win in their second innings" _match.TeamB (formatRuns (a1.GetRuns + a2.GetRuns - b1.GetRuns))
+        | A_Ongoing a1 -> sprintf "%s are %i for %i in their first innings" _match.TeamA (TotalRunsA state) a1.GetWickets
+        | A_Completed a1 when a1.IsDeclared -> sprintf "%s scored %i for %i declared in their first innings" _match.TeamA (TotalRunsA state) a1.GetWickets
+        | A_Completed a1 -> sprintf "%s scored %i all out in their first innings" _match.TeamA (TotalRunsA state)
+        | AB_Ongoing (a1, b1) & ALeads -> sprintf "%s trail by %s with %s remaining in their first innings" _match.TeamB (formatRuns (LeadA state)) (formatWicketsLeft b1.GetWickets)
+        | AB_Ongoing (a1, b1) & ScoresLevel -> sprintf "%s are level with %s remaining in their first innings" _match.TeamB (formatWicketsLeft b1.GetWickets)
+        | AB_Ongoing (a1, b1) & BLeads -> sprintf "%s lead by %s with %s remaining in their first innings" _match.TeamB (formatRuns (LeadB state)) (formatWicketsLeft b1.GetWickets)
+        | (AB_CompletedNoFollowOn (a1, b1) | AB_CompletedPossibleFollowOn (a1, b1)) & ALeads -> sprintf "%s lead by %s after the first innings" _match.TeamA (formatRuns (LeadA state))
+        | (AB_CompletedNoFollowOn (a1, b1) | AB_CompletedPossibleFollowOn (a1, b1)) & ScoresLevel -> sprintf "%s are level after the first innings" _match.TeamB
+        | (AB_CompletedNoFollowOn (a1, b1) | AB_CompletedPossibleFollowOn (a1, b1)) & BLeads -> sprintf "%s lead by %s after the first innings" _match.TeamB (formatRuns (LeadB state))
+        | ABA_Ongoing (a1, b1, a2) & BLeads -> sprintf "%s trail by %s with %s remaining in their second innings" _match.TeamA (formatRuns (LeadB state)) (formatWicketsLeft a2.GetWickets)
+        | ABA_Ongoing (a1, b1, a2) & ScoresLevel -> sprintf "%s are level with %s remaining in their second innings" _match.TeamA (formatWicketsLeft a2.GetWickets)
+        | ABA_Ongoing (a1, b1, a2) & ALeads -> sprintf "%s lead by %s with %s remaining in their second innings" _match.TeamA (formatRuns (LeadA state)) (formatWicketsLeft a2.GetWickets)
+        | ABA_VictoryB _ -> sprintf "%s won by %s" _match.TeamB (formatRuns (LeadB state))
+        | ABA_Completed _ -> sprintf "%s need %s to win in their second innings" _match.TeamB (formatRuns (LeadA state))
         | _ -> failwith "not implemented"
--- CricketEngine/MatchState.fs
+++ CricketEngine/MatchState.fs
@@ -1,6 +1,6 @@
 ﻿namespace Cricket.CricketEngine
 
-type MatchState =
+type MatchState = 
     | NotStarted
     | Abandoned
     | A_Ongoing of Innings
@@ -30,91 +30,92 @@ type MatchState =
     | ABBA_MatchTied of Innings * Innings * Innings * Innings
 
 [<AutoOpen>]
-module MatchStateFunctions =
-    
-//    let (|MatchNotStarted|MatchAbandoned|MatchDrawn|MatchVictoryA|MatchVictoryB|MatchTied|MatchOngoing|) state =
-//        match state with
-//        | NotStarted -> MatchNotStarted
-//        | Abandoned -> MatchAbandoned
-//        | A_MatchDrawn _ | AB_MatchDrawn _ | ABA_MatchDrawn _ | ABB_MatchDrawn _ | ABAB_MatchDrawn _ | ABBA_MatchDrawn _ -> MatchDrawn
-//        | ABB_VictoryA _ | ABAB_VictoryA _ | ABBA_VictoryA _ -> MatchVictoryA
-//        | ABA_VictoryB _ | ABAB_VictoryB _ | ABBA_VictoryB _ -> MatchVictoryB
-//        | ABAB_MatchTied _ | ABBA_MatchTied _ -> MatchTied
-//        | _ -> MatchOngoing
-//
-//    let (|NotStarted|Completed|Ongoing|) state =
-//        match state with
-//        | MatchNotStarted -> NotStarted
-//        | MatchOngoing -> Ongoing
-//        | _ -> Completed
-
-    let StartMatch rules state =
+module MatchStateTransitions = 
+    //    let (|MatchNotStarted|MatchAbandoned|MatchDrawn|MatchVictoryA|MatchVictoryB|MatchTied|MatchOngoing|) state =
+    //        match state with
+    //        | NotStarted -> MatchNotStarted
+    //        | Abandoned -> MatchAbandoned
+    //        | A_MatchDrawn _ | AB_MatchDrawn _ | ABA_MatchDrawn _ | ABB_MatchDrawn _ | ABAB_MatchDrawn _ | ABBA_MatchDrawn _ -> MatchDrawn
+    //        | ABB_VictoryA _ | ABAB_VictoryA _ | ABBA_VictoryA _ -> MatchVictoryA
+    //        | ABA_VictoryB _ | ABAB_VictoryB _ | ABBA_VictoryB _ -> MatchVictoryB
+    //        | ABAB_MatchTied _ | ABBA_MatchTied _ -> MatchTied
+    //        | _ -> MatchOngoing
+    //
+    //    let (|NotStarted|Completed|Ongoing|) state =
+    //        match state with
+    //        | MatchNotStarted -> NotStarted
+    //        | MatchOngoing -> Ongoing
+    //        | _ -> Completed
+    let StartMatch rules state = 
         match state with
-        | NotStarted -> A_Ongoing (Innings (0, 0, false))
+        | NotStarted -> A_Ongoing(Innings(0, 0, false))
         | _ -> failwith "Call to StartMatch in invalid state"
-
-    let AbandonMatch rules state =
+    
+    let AbandonMatch rules state = 
         match state with
         | NotStarted -> Abandoned
         | _ -> failwith "Call to AbandonMatch in invalid state"
-
-    let DrawMatch rules state =
+    
+    let DrawMatch rules state = 
         match state with
-        | A_Ongoing (a1) | A_Completed (a1) -> A_MatchDrawn (a1)
-        | AB_Ongoing (a1, b1) | AB_CompletedNoFollowOn (a1, b1) | AB_CompletedPossibleFollowOn (a1, b1) -> AB_MatchDrawn (a1, b1)
-        | ABA_Ongoing (a1, b1, a2) | ABA_Completed (a1, b1, a2) -> ABA_MatchDrawn (a1, b1, a2)
-        | ABB_Ongoing (a1, b1, b2) | ABB_Completed (a1, b1, b2) -> ABB_MatchDrawn (a1, b1, b2)
-        | ABAB_Ongoing (a1, b1, a2, b2) -> ABAB_MatchDrawn (a1, b1, a2, b2)
-        | ABBA_Ongoing (a1, b1, b2, a2) -> ABBA_MatchDrawn (a1, b1, b2, a2)
+        | A_Ongoing(a1) | A_Completed(a1) -> A_MatchDrawn(a1)
+        | AB_Ongoing(a1, b1) | AB_CompletedNoFollowOn(a1, b1) | AB_CompletedPossibleFollowOn(a1, b1) -> 
+            AB_MatchDrawn(a1, b1)
+        | ABA_Ongoing(a1, b1, a2) | ABA_Completed(a1, b1, a2) -> ABA_MatchDrawn(a1, b1, a2)
+        | ABB_Ongoing(a1, b1, b2) | ABB_Completed(a1, b1, b2) -> ABB_MatchDrawn(a1, b1, b2)
+        | ABAB_Ongoing(a1, b1, a2, b2) -> ABAB_MatchDrawn(a1, b1, a2, b2)
+        | ABBA_Ongoing(a1, b1, b2, a2) -> ABBA_MatchDrawn(a1, b1, b2, a2)
         | _ -> failwith "Call to DrawMatch in invalid state"
-
-    let EnforceFollowOn rules state =
+    
+    let EnforceFollowOn rules state = 
         match state with
-        | AB_CompletedPossibleFollowOn (a1, b1) -> ABB_Ongoing (a1, b1, Innings (0, 0, false))
+        | AB_CompletedPossibleFollowOn(a1, b1) -> ABB_Ongoing(a1, b1, Innings(0, 0, false))
         | _ -> failwith "Call to EnforceFollowOn in invalid state"
-
-    let DeclineFollowOn rules state =
+    
+    let DeclineFollowOn rules state = 
         match state with
-        | AB_CompletedPossibleFollowOn (a1, b1) -> ABA_Ongoing (a1, b1, Innings (0, 0, false))
+        | AB_CompletedPossibleFollowOn(a1, b1) -> ABA_Ongoing(a1, b1, Innings(0, 0, false))
         | _ -> failwith "Call to DeclineFollowOn in invalid state"
     
-    let UpdateInnings rules inningsUpdater state =
+    let UpdateInnings rules inningsUpdater state = 
         match state with
-        | A_Ongoing (a1) ->
+        | A_Ongoing(a1) -> 
             match inningsUpdater a1 with
-            | InningsOngoing i -> A_Ongoing (i)
-            | InningsCompleted i -> A_Completed (i)
-        | AB_Ongoing (a1, b1) ->
+            | InningsOngoing i -> A_Ongoing(i)
+            | InningsCompleted i -> A_Completed(i)
+        | AB_Ongoing(a1, b1) -> 
             match inningsUpdater b1 with
-            | InningsOngoing i -> AB_Ongoing (a1, i)
-            | InningsCompleted i when (a1.GetRuns >= i.GetRuns + rules.FollowOnMargin) -> AB_CompletedPossibleFollowOn (a1, i)
-            | InningsCompleted i -> AB_CompletedNoFollowOn (a1, i)
-        | ABA_Ongoing (a1, b1, a2) ->
+            | InningsOngoing i -> AB_Ongoing(a1, i)
+            | InningsCompleted i when (a1.GetRuns >= i.GetRuns + rules.FollowOnMargin) -> 
+                AB_CompletedPossibleFollowOn(a1, i)
+            | InningsCompleted i -> AB_CompletedNoFollowOn(a1, i)
+        | ABA_Ongoing(a1, b1, a2) -> 
             match inningsUpdater a2 with
-            | InningsOngoing i -> ABA_Ongoing (a1, b1, i)
-            | InningsCompleted i when (b1.GetRuns > a1.GetRuns + i.GetRuns) -> ABA_VictoryB (a1, b1, i)
-            | InningsCompleted i -> ABA_Completed (a1, b1, i)
-        | ABB_Ongoing (a1, b1, b2) ->
+            | InningsOngoing i -> ABA_Ongoing(a1, b1, i)
+            | InningsCompleted i when (b1.GetRuns > a1.GetRuns + i.GetRuns) -> ABA_VictoryB(a1, b1, i)
+            | InningsCompleted i -> ABA_Completed(a1, b1, i)
+        | ABB_Ongoing(a1, b1, b2) -> 
             match inningsUpdater b2 with
-            | InningsOngoing i -> ABB_Ongoing (a1, b1, i)
-            | InningsCompleted i when (a1.GetRuns > b1.GetRuns + i.GetRuns) -> ABB_VictoryA (a1, b1, i)
-            | InningsCompleted i -> ABB_Completed (a1, b1, i)
-        | ABAB_Ongoing (a1, b1, a2, b2) ->
+            | InningsOngoing i -> ABB_Ongoing(a1, b1, i)
+            | InningsCompleted i when (a1.GetRuns > b1.GetRuns + i.GetRuns) -> ABB_VictoryA(a1, b1, i)
+            | InningsCompleted i -> ABB_Completed(a1, b1, i)
+        | ABAB_Ongoing(a1, b1, a2, b2) -> 
             match inningsUpdater b2 with
-            | InningsOngoing i when (b1.GetRuns + i.GetRuns > a1.GetRuns + a2.GetRuns) -> ABAB_VictoryB (a1, b1, a2, i)
-            | InningsOngoing i -> ABAB_Ongoing (a1, b1, a2, i)
-            | InningsCompleted i when (a1.GetRuns + a2.GetRuns > b1.GetRuns + i.GetRuns) -> ABAB_VictoryA (a1, b1, a2, i)
-            | InningsCompleted i when (a1.GetRuns + a2.GetRuns = b1.GetRuns + i.GetRuns) -> ABAB_MatchTied (a1, b1, a2, i)
-            | InningsCompleted i -> failwith "Call to UpdateInnings in inconsistent state"
-        | ABBA_Ongoing (a1, b1, b2, a2) ->
+            | InningsOngoing i when (b1.GetRuns + i.GetRuns > a1.GetRuns + a2.GetRuns) -> ABAB_VictoryB(a1, b1, a2, i)
+            | InningsOngoing i -> ABAB_Ongoing(a1, b1, a2, i)
+            | InningsCompleted i when (a1.GetRuns + a2.GetRuns > b1.GetRuns + i.GetRuns) -> ABAB_VictoryA(a1, b1, a2, i)
+            | InningsCompleted i when (a1.GetRuns + a2.GetRuns = b1.GetRuns + i.GetRuns) -> 
+                ABAB_MatchTied(a1, b1, a2, i)
+            | InningsCompleted _ -> failwith "Call to UpdateInnings in inconsistent state"
+        | ABBA_Ongoing(a1, b1, b2, a2) -> 
             match inningsUpdater a2 with
-            | InningsOngoing i when (a1.GetRuns + i.GetRuns > b1.GetRuns + b2.GetRuns) -> ABBA_VictoryA (a1, b1, b2, i)
-            | InningsOngoing i -> ABBA_Ongoing (a1, b1, b2, i)
-            | InningsCompleted i when (b1.GetRuns + b2.GetRuns > a1.GetRuns + i.GetRuns) -> ABBA_VictoryB (a1, b1, b2, i)
-            | InningsCompleted i when (b1.GetRuns + b2.GetRuns = a1.GetRuns + i.GetRuns) -> ABBA_MatchTied (a1, b1, b2, i)
-            | InningsCompleted i -> failwith "Call to UpdateInnings in inconsistent state"
+            | InningsOngoing i when (a1.GetRuns + i.GetRuns > b1.GetRuns + b2.GetRuns) -> ABBA_VictoryA(a1, b1, b2, i)
+            | InningsOngoing i -> ABBA_Ongoing(a1, b1, b2, i)
+            | InningsCompleted i when (b1.GetRuns + b2.GetRuns > a1.GetRuns + i.GetRuns) -> ABBA_VictoryB(a1, b1, b2, i)
+            | InningsCompleted i when (b1.GetRuns + b2.GetRuns = a1.GetRuns + i.GetRuns) -> 
+                ABBA_MatchTied(a1, b1, b2, i)
+            | InningsCompleted _ -> failwith "Call to UpdateInnings in inconsistent state"
         | _ -> failwith "Call to UpdateInnings in invalid state"
-
 
 //        | NotStarted
 //        | Abandoned
@@ -144,4 +145,74 @@ module MatchStateFunctions =
 //        | ABBA_MatchDrawn (a1, b1, b2, a2)
 //        | ABBA_MatchTied (a1, b1, b2, a2)
 
+[<AutoOpen>]
+module MatchStateFunctions = 
+    let TotalRunsA state = 
+        match state with
+        | NotStarted | Abandoned -> 0
+        | A_Ongoing a1
+            | A_Completed a1
+            | A_MatchDrawn a1 -> a1.GetRuns
+        | AB_Ongoing(a1, b1)
+            | AB_CompletedNoFollowOn(a1, b1)
+            | AB_CompletedPossibleFollowOn(a1, b1)
+            | AB_MatchDrawn(a1, b1) -> a1.GetRuns
+        | ABA_Ongoing(a1, b1, a2)
+            | ABA_VictoryB(a1, b1, a2)
+            | ABA_Completed(a1, b1, a2)
+            | ABA_MatchDrawn(a1, b1, a2) -> a1.GetRuns + a2.GetRuns
+        | ABB_Ongoing(a1, b1, b2)
+            | ABB_VictoryA(a1, b1, b2)
+            | ABB_Completed(a1, b1, b2)
+            | ABB_MatchDrawn(a1, b1, b2) -> a1.GetRuns
+        | ABAB_Ongoing(a1, b1, a2, b2)
+            | ABAB_VictoryA(a1, b1, a2, b2)
+            | ABAB_VictoryB(a1, b1, a2, b2)
+            | ABAB_MatchDrawn(a1, b1, a2, b2)
+            | ABAB_MatchTied(a1, b1, a2, b2)
+            | ABBA_Ongoing(a1, b1, b2, a2)
+            | ABBA_VictoryA(a1, b1, b2, a2)
+            | ABBA_VictoryB(a1, b1, b2, a2)
+            | ABBA_MatchDrawn(a1, b1, b2, a2)
+            | ABBA_MatchTied(a1, b1, b2, a2) -> a1.GetRuns + a2.GetRuns
 
+    let TotalRunsB state = 
+        match state with
+        | NotStarted | Abandoned -> 0
+        | A_Ongoing a1
+            | A_Completed a1
+            | A_MatchDrawn a1 -> 0
+        | AB_Ongoing(a1, b1)
+            | AB_CompletedNoFollowOn(a1, b1)
+            | AB_CompletedPossibleFollowOn(a1, b1)
+            | AB_MatchDrawn(a1, b1) -> b1.GetRuns
+        | ABA_Ongoing(a1, b1, a2)
+            | ABA_VictoryB(a1, b1, a2)
+            | ABA_Completed(a1, b1, a2)
+            | ABA_MatchDrawn(a1, b1, a2) -> b1.GetRuns
+        | ABB_Ongoing(a1, b1, b2)
+            | ABB_VictoryA(a1, b1, b2)
+            | ABB_Completed(a1, b1, b2)
+            | ABB_MatchDrawn(a1, b1, b2) -> b1.GetRuns + b2.GetRuns
+        | ABAB_Ongoing(a1, b1, a2, b2)
+            | ABAB_VictoryA(a1, b1, a2, b2)
+            | ABAB_VictoryB(a1, b1, a2, b2)
+            | ABAB_MatchDrawn(a1, b1, a2, b2)
+            | ABAB_MatchTied(a1, b1, a2, b2)
+            | ABBA_Ongoing(a1, b1, b2, a2)
+            | ABBA_VictoryA(a1, b1, b2, a2)
+            | ABBA_VictoryB(a1, b1, b2, a2)
+            | ABBA_MatchDrawn(a1, b1, b2, a2)
+            | ABBA_MatchTied(a1, b1, b2, a2) -> b1.GetRuns + b2.GetRuns
+
+    let LeadA state =
+        (TotalRunsA state) - (TotalRunsB state)
+
+    let LeadB state =
+        (TotalRunsB state) - (TotalRunsA state)
+
+    let (|ALeads|ScoresLevel|BLeads|) state =
+        match (LeadA state) with
+        | x when x > 0 -> ALeads
+        | x when x < 0 -> BLeads
+        | _ -> ScoresLevel
