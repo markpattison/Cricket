904ce846661f00b621f2f40156ba7720ad92f52a
markpattison <mr.pattison@gmail.com>
2017-10-09 14:52:04 +0100

    added dotnetcli install to build script

--- build.fsx
+++ build.fsx
@@ -7,8 +7,6 @@ open Fake
 open Fake.FileUtils
 open Fake.YarnHelper
 
-// Directories
-
 // Filesets
 let appReferences = 
     !! "**/*.csproj"
@@ -29,7 +27,14 @@ let acceptanceTestReferences =
     !! "**/*AcceptanceTests.csproj"
         ++ "**/*AcceptanceTests.fsproj"
 
+let dotnetcliVersion = "2.0.0"
+let mutable dotnetExePath = "dotnet"
+
 // Targets
+Target "InstallDotNetCore" (fun _ ->
+    dotnetExePath <- DotNetCli.InstallDotNetSDK dotnetcliVersion
+)
+
 Target "Clean" (fun _ ->
     [ appReferences; unitTestReferences; acceptanceTestReferences ]
     |> Seq.concat
@@ -39,28 +44,28 @@ Target "Clean" (fun _ ->
 Target "Restore" (fun _ ->
     [ appReferences; unitTestReferences; acceptanceTestReferences; fableReferences ]
     |> Seq.concat
-    |> Seq.iter (fun proj -> DotNetCli.Restore (fun p -> { p with Project = proj }))
+    |> Seq.iter (fun proj -> DotNetCli.Restore (fun p -> { p with Project = proj; ToolPath = dotnetExePath }))
 )
 
 Target "BuildApp" (fun _ ->
     appReferences
-    |> Seq.iter (fun proj -> DotNetCli.Build (fun p -> { p with Project = proj }))
+    |> Seq.iter (fun proj -> DotNetCli.Build (fun p -> { p with Project = proj; ToolPath = dotnetExePath }))
 )
 
 Target "BuildTests" (fun _ ->
     [ unitTestReferences; acceptanceTestReferences ]
     |> Seq.concat
-    |> Seq.iter (fun proj -> DotNetCli.Build (fun p -> { p with Project = proj }))
+    |> Seq.iter (fun proj -> DotNetCli.Build (fun p -> { p with Project = proj; ToolPath = dotnetExePath }))
 )
 
 Target "RunUnitTests" (fun _ ->
     unitTestReferences
-    |> Seq.iter (fun proj -> DotNetCli.Test (fun p -> { p with Project = proj }))
+    |> Seq.iter (fun proj -> DotNetCli.Test (fun p -> { p with Project = proj; ToolPath = dotnetExePath }))
 )
 
 Target "RunAcceptanceTests" (fun _ ->
     acceptanceTestReferences
-    |> Seq.iter (fun proj -> DotNetCli.Test (fun p -> { p with Project = proj }))
+    |> Seq.iter (fun proj -> DotNetCli.Test (fun p -> { p with Project = proj; ToolPath = dotnetExePath }))
 )
 
 Target "YarnInstall" (fun _ ->
@@ -70,16 +75,17 @@ Target "YarnInstall" (fun _ ->
 
 Target "BuildFable" (fun _ ->
     fableReference
-    |> (fun proj -> DotNetCli.RunCommand (fun p -> { p with WorkingDir = fableDirectory }) ("fable yarn-build " + proj))
+    |> (fun proj -> DotNetCli.RunCommand (fun p -> { p with WorkingDir = fableDirectory; ToolPath = dotnetExePath }) ("fable yarn-build " + proj))
 )
 
 Target "RunFable" (fun _ ->
     fableReference
-    |> (fun proj -> DotNetCli.RunCommand (fun p -> { p with WorkingDir = fableDirectory }) ("fable yarn-start " + proj))
+    |> (fun proj -> DotNetCli.RunCommand (fun p -> { p with WorkingDir = fableDirectory; ToolPath = dotnetExePath }) ("fable yarn-start " + proj))
 )
 
 // Build order
-"Clean"
+"InstallDotNetCore"
+    ==> "Clean"
     ==> "Restore"
     ==> "BuildApp"
     ==> "BuildTests"
