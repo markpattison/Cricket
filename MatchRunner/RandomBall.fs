5743036463d88a82a3c2eae12cdc49050231d559
markpattison <mr.pattison@gmail.com>
2018-01-14 12:23:20 +0000

    initial thoughts on calibrating ball outcomes

--- MatchRunner/RandomBall.fs
+++ MatchRunner/RandomBall.fs
@@ -16,3 +16,35 @@ module RandomBall =
         | x when x >= 95 -> ScoreRuns 2
         | x when x >= 3 -> ScoreRuns 1
         | _ -> DotBall
+
+
+// Thoughts on generating the ball outcome
+// ***************************************
+//
+// Could depend on:
+//  - batsman's skill
+//  - batsman's consistency
+//  - batsman's long-term form
+//  - batsman's short-term form e.g. has got his eye in
+//  - batsman's tiredness
+//  - batsman's natural level of aggression
+//  - batsman's level of intended aggression based on match situation
+//
+//  - bowler's skill
+//  - bowler's consistency
+//  - bowler's long-term form
+//  - bowler's short-term form
+//  - bowler's tiredness (greater effect than for batsmen)
+//  - bowler's natural level of aggression
+//  - bowler's level of intended aggression based on match situation
+//
+//  Initially, could just use skill levels
+//
+// Calibration thoughts:
+//  - E(runs per ball) increases (slowly) with batsman's skill and decreases (quickly) with bowler's skill
+//  - E(wickets per ball) decreases (quickly) with batsman's skill and increases (slowly) with bowler's skill
+//  - E(runs per wicket) must increase with batsman's skill and decrease with bowler's skill
+//
+//  - E(runs per ball) increases with aggression
+//  - E(wickets per ball) increases with aggression
+//  - E(runs per wicket) is concave with aggression
