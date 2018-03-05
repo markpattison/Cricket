37fc2874e69632824d46fc9fd63234d40e035aeb
markpattison <mr.pattison@gmail.com>
2018-03-05 21:51:19 +0000

    Fulma WIP

--- src/FableCricket/Info/View.fs
+++ src/FableCricket/Info/View.fs
@@ -1,19 +1,19 @@
 module FableCricket.Info.View
 
 open Fable.Helpers.React
-open Fable.Helpers.React.Props
+
+open Fulma.Elements
 
 open FableCricket
 
 let root =
-  div
-    [ ClassName "content" ]
+  Content.content []
     [ h1
         [ ]
         [ str "About FableCricket" ]
       p
         [ ]
-        [ str "This is a simple cricket game built with F# + Fable + Elmish + React." ]
+        [ str "This is a simple cricket game built with F# + Fable + Elmish + React + Fulma." ]
       p
         [ ]
         [ str ("Version " + App.Version.VersionNumber) ] ]
--- src/FableCricket/Navbar/View.fs
+++ src/FableCricket/Navbar/View.fs
@@ -3,37 +3,44 @@ module FableCricket.Navbar.View
 open Fable.Helpers.React
 open Fable.Helpers.React.Props
 
+open Fulma.Color
+open Fulma.Components
+open Fulma.Elements
+open Fulma.Elements.Form
+open Fulma.Extra.FontAwesome
+open Fulma.Size
+
 let navButton classy href faClass txt =
-    p
-        [ ClassName "control" ]
-        [ a
-            [ ClassName (sprintf "button %s" classy)
-              Href href ]
-            [ span
-                [ ClassName "icon" ]
-                [ i
-                    [ ClassName (sprintf "fa %s" faClass) ]
-                    [ ] ]
-              span
-                [ ]
-                [ str txt ] ] ]
+  Control.div
+    [
+      Control.CustomClass (sprintf "button %s" classy)
+      Control.Props [ Href href ]
+    ]
+    [
+      Icon.faIcon [] [ Fa.icon faClass ]
+      span [] [ str txt ]
+    ]
 
 let navButtons =
-    span
-        [ ClassName "nav-item" ]
-        [ div
-            [ ClassName "field is-grouped" ]
-            [ navButton "twitter" "https://twitter.com/mark_pattison" "fa-twitter" "Twitter"
-              navButton "github" "https://github.com/markpattison/Cricket" "fa-github" "Fork me" ] ]
+  Navbar.Item.div []
+    [ Field.div
+        [ Field.IsGrouped ]
+        [
+          navButton "twitter" "https://twitter.com/mark_pattison" Fa.I.Twitter "Twitter"
+          navButton "github" "https://github.com/markpattison/Cricket" Fa.I.Github "Github"
+        ]
+    ]
 
 let root =
-    nav
-        [ ClassName "nav" ]
-        [ div
-            [ ClassName "nav-left" ]
-            [ h1
-                [ ClassName "nav-item is-brand title is-4" ]
-                [ str "FableCricket" ] ]
-          div
-            [ ClassName "nav-right" ]
-            [ navButtons ] ]
+  Navbar.navbar [ Navbar.Color IsPrimary ]
+    [
+      Navbar.Brand.div []
+        [
+          Navbar.Item.div []
+            [
+              Heading.h4 [] [ str "FableCricket" ]
+            ]
+        ]
+      Navbar.End.div []
+        [ navButtons ]
+    ]
