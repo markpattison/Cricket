75183873e9b91ac72806bd9c3c2b614f8bf9d7ba
markpattison <mr.pattison@gmail.com>
2018-09-18 20:26:11 +0100

    test

--- src/FableCricket/Navbar/View.fs
+++ src/FableCricket/Navbar/View.fs
@@ -25,6 +25,6 @@ let root =
   Navbar.navbar [ Navbar.Color IsPrimary ]
     [ Navbar.Brand.div []
         [ Navbar.Item.div []
-            [ Heading.h4 [] [ str "FableCricket" ] ] ]
+            [ Heading.h4 [] [ str "FableCricket TEST" ] ] ]
       Navbar.End.div []
         [ navButtons ] ]
