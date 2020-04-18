c7d8d0556dd4401d9bf40302714bd342f7b038d8
markpattison <mr.pattison@gmail.com>
2020-04-18 16:35:48 +0100

    move team data to MatchRunner

--- src/FableCricket/LiveMatch/State.fs
+++ src/FableCricket/LiveMatch/State.fs
@@ -7,61 +7,9 @@ open Elmish
 
 open Types
 
-let players =
-    [
-        "GA Gooch",      1.00, 0.50
-        "MA Atherton",   1.05, 0.60
-        "DI Gower",      1.00, 0.50
-        "AJ Lamb",       0.90, 0.50
-        "RA Smith",      0.95, 0.50
-        "JE Morris",     0.90, 0.50
-        "RC Russell",    0.85, 0.50
-        "CC Lewis",      0.80, 0.70
-        "EE Hemmings",   0.70, 0.65
-        "ARC Fraser",    0.65, 0.75
-        "DE Malcolm",    0.60, 0.70
-        "RJ Shastri",    0.95, 0.70
-        "NS Sidhu",      0.90, 0.70
-        "SV Manjrekar",  1.00, 0.70
-        "DB Vengsarkar", 0.90, 0.70
-        "M Azharuddin",  1.00, 0.70
-        "SR Tendulkar",  1.05, 0.70
-        "M Prabhakar",   0.90, 0.70
-        "N Kapil Dev",   0.85, 0.75
-        "KS More",       0.80, 0.70
-        "SK Sharma",     0.70, 0.65
-        "ND Hirwani",    0.65, 0.70
-    ]
-
-let newMatch =
-    {
-        TeamA =
-            {
-                TeamName = "England"
-                Players =
-                    players
-                    |> List.take 11
-                    |> List.mapi (fun n (name, _, _) -> { Name = name; ID = n })
-                    |> List.toArray
-            }
-        TeamB =
-            {
-                TeamName = "India"
-                Players =
-                    players
-                    |> List.skip 11
-                    |> List.take 11
-                    |> List.mapi (fun n (name, _, _) -> { Name = name; ID = 11 + n })
-                    |> List.toArray
-            }
-        State = NotStarted
-        Rules = { FollowOnMargin = 200 }
-    }
-
-
 let init () : Model * Cmd<Msg> =
     {
-        Match = newMatch
+        Match = MatchData.newMatch
         PlayerRecords = Map.empty
         LivePlayerRecords = Map.empty
         InningsExpanded = []
@@ -69,7 +17,7 @@ let init () : Model * Cmd<Msg> =
         PlayerAttributes =
             {
                 Attributes =
-                    players
+                    MatchData.players
                     |> List.mapi (fun n (_, bat, bowl) -> n, { BattingSkill = bat; BowlingSkill = bowl })
                     |> Map.ofList
             }
@@ -85,7 +33,7 @@ let checkForNewInnings model =
         model
 
 let update msg model =
-    let updateMatch f =
+    let updateModel f =
         let updated =
             let alreadyCompleted = Match.isCompleted model.Match
             let updatedMatch = f model.Match
@@ -111,12 +59,12 @@ let update msg model =
             |> checkForNewInnings
         updated, []
     match msg with
-    | ContinueInningsBallMessage -> updateMatch (MatchRunner.continueInningsBall model.PlayerAttributes)
-    | ContinueInningsOverMessage -> updateMatch (MatchRunner.continueInningsOver model.PlayerAttributes)
-    | ContinueInningsInningsMessage -> updateMatch (MatchRunner.continueInningsInnings model.PlayerAttributes)
-    | StartMatchMessage -> updateMatch (MatchRunner.updateMatchState MatchUpdate.StartMatch)
-    | StartNextInningsMessage -> updateMatch (MatchRunner.updateMatchState MatchUpdate.StartNextInnings)
-    | ResetMatchMessage -> { model with Match = newMatch; InningsExpanded = [] }, []
+    | ContinueInningsBallMessage -> updateModel (MatchRunner.continueInningsBall model.PlayerAttributes)
+    | ContinueInningsOverMessage -> updateModel (MatchRunner.continueInningsOver model.PlayerAttributes)
+    | ContinueInningsInningsMessage -> updateModel (MatchRunner.continueInningsInnings model.PlayerAttributes)
+    | StartMatchMessage -> updateModel (MatchRunner.updateMatchState MatchUpdate.StartMatch)
+    | StartNextInningsMessage -> updateModel (MatchRunner.updateMatchState MatchUpdate.StartNextInnings)
+    | ResetMatchMessage -> { model with Match = MatchData.newMatch; InningsExpanded = [] }, []
     | ToggleInningsExpandedMessage index ->
         { model with
             InningsExpanded = model.InningsExpanded |> List.mapi
