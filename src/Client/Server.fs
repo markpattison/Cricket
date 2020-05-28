2706ca17dc2c2624d0be317f922f75874b939d8d
markpattison <mr.pattison@gmail.com>
2020-05-28 22:46:19 +0100

    deal with virtual path when calling api

--- src/Client/Server.fs
+++ src/Client/Server.fs
@@ -1,9 +1,37 @@
 module Cricket.Client.Server
 
+open System
+
 open Cricket.Shared
+open Fable.Core
 open Fable.Remoting.Client
+
+// for details of how to deal with virtual path, see:
+// https://github.com/Zaid-Ajaj/SAFE.Simplified/blob/master/client/src/Server.fs
+
+let virtualPath : string =
+    #if MOCHA_TESTS
+    "/"
+    #else
+    JS.eval("window.location.pathname")
+    #endif
+
+let combine (paths: string list) =
+    paths
+    |> List.map (fun path -> List.ofArray (path.Split('/')))
+    |> List.concat
+    |> List.filter (fun segment -> not (segment.Contains(".")))
+    |> List.filter (String.IsNullOrWhiteSpace >> not)
+    |> String.concat "/"
+    |> sprintf "/%s"
+
+let normalize (path: string) = combine [ virtualPath; path ]
+
+let normalizeRoutes typeName methodName =
+    Route.builder typeName methodName
+    |> normalize
 
 let api : ICricketApi =
     Remoting.createApi()
-    |> Remoting.withRouteBuilder Route.builder
+    |> Remoting.withRouteBuilder normalizeRoutes
     |> Remoting.buildProxy<ICricketApi>
