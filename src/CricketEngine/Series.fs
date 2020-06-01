239cd51a0730328baa786f105ed3070056e56a11
markpattison <mr.pattison@gmail.com>
2020-06-01 15:39:00 +0100

    store match summaries in series

--- src/CricketEngine/Series.fs
+++ src/CricketEngine/Series.fs
@@ -1,7 +1,14 @@
 namespace Cricket.CricketEngine
 
+type MatchRecord =
+    {
+        Index: int
+        Summary: string
+    }
+
 type Series =
     {
+        CompletedMatches: MatchRecord list
         Team1: string
         Team2: string
         Team1Wins: int
@@ -16,6 +23,7 @@ module Series =
 
     let create team1 team2 =
         {
+            CompletedMatches = []
             Team1 = team1
             Team2 = team2
             Team1Wins = 0
@@ -24,17 +32,26 @@ module Series =
             Ties = 0
         }
     
-
-    let update series result batFirst =
-        match result, batFirst with
-        | WinTeamA, Team1 | WinTeamB, Team2 -> { series with Team1Wins = series.Team1Wins + 1 }
-        | WinTeamB, Team1 | WinTeamA, Team2 -> { series with Team2Wins = series.Team2Wins + 1 }
-        | Draw, _ -> { series with Draws = series.Draws + 1 }
-        | Tie, _ -> { series with Ties = series.Ties + 1 }
-        | NoResult, _ -> series
-    
     let totalMatches series =
-        series.Team1Wins + series.Team2Wins + series.Draws + series.Ties
+        series.Team1Wins + series.Team2Wins + series.Draws + series.Ties    
+
+    let update series mtch batFirst =
+        let summary = Match.summaryStatus mtch
+        let index = 1 + totalMatches series
+
+        let completed = List.append series.CompletedMatches [ { Index = index; Summary = summary } ]
+
+        let result =
+            match MatchState.summaryState mtch.State with
+            | MatchCompleted result -> result
+            | _ -> NoResult
+        
+        match result, batFirst with
+        | WinTeamA, Team1 | WinTeamB, Team2 -> { series with CompletedMatches = completed; Team1Wins = series.Team1Wins + 1 }
+        | WinTeamB, Team1 | WinTeamA, Team2 -> { series with CompletedMatches = completed; Team2Wins = series.Team2Wins + 1 }
+        | Draw, _ -> { series with CompletedMatches = completed; Draws = series.Draws + 1 }
+        | Tie, _ -> { series with CompletedMatches = completed; Ties = series.Ties + 1 }
+        | NoResult, _ -> { series with CompletedMatches = completed }
 
     let summary series =
         let wins =
